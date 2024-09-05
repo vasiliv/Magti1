11859baa037d55a9852b2e667fcbe3bb24269ba3
vasiliv <32122468+vasiliv@users.noreply.github.com>
2024-09-05 13:37:06 +0400

    Add project files.

---
+++ Magti1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Magti1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
