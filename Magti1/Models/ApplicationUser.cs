44b534c3a1e26fc99b02b71fe01a6a46d8dffc59
vasiliv <32122468+vasiliv@users.noreply.github.com>
2024-09-05 14:34:49 +0400

    v1 - APplicationUser class created

---
+++ Magti1/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Magti1.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    //To Make AspNetUsers Id int, not guid
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public string? PersonalIDNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? ImageFileName { get; set; }
+    }
+}
