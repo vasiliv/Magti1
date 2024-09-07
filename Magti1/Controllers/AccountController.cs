e6b4fb106e1a9826cf726e70da1909341473201e
vasiliv <32122468+vasiliv@users.noreply.github.com>
2024-09-07 23:09:56 +0400

    v7 - Registration finished

--- Magti1/Controllers/AccountController.cs
+++ Magti1/Controllers/AccountController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Magti1.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Magti1.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +26,38 @@ namespace Magti1.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                //perform registration
+                ApplicationUser user = new()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    UserName = model.PersonalIDNumber,
+                    PersonalIDNumber = model.PersonalIDNumber,
+                    Email = model.Email,
+                    Address = model.Address,
+                };
+                var result = await _userManager.CreateAsync(user, model.Password!);
+                //move to the next page
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    //Go to Home Controller Index Action
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var error in result.Errors)
+                {
+                    //key-value pair
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View();
+        }
+
         public async Task<IActionResult> LoginOut()
         {
             return View();
